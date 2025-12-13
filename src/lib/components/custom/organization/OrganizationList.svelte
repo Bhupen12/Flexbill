4d851ad53be78b3f414cadce70d4c00031559744
bhupen <bhupensahoo200127@gmail.com>
2025-12-13 14:05:02 +0530

    Organization List

---
+++ src/lib/components/custom/organization/OrganizationList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { organizationsApi } from "$lib/api";
+	import type { OrganizationSelect } from "$lib/types";
+	import { onMount } from "svelte";
+
+  let organizations: OrganizationSelect[] = [];
+
+  async function fetchAllOrganizations() {
+    organizations = await organizationsApi.list();
+  }
+
+  onMount(()=>{
+    fetchAllOrganizations();
+  })
+</script>
+
+{JSON.stringify(organizations, null, 2)}
