72729423022f190bf09168b2f4d1b40dee2ae8b2
bhupen <bhupensahoo200127@gmail.com>
2025-12-15 15:39:38 +0530

    eslint error disable

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { ClaimType } from "$lib/types";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			claims: ClaimType
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
