1e10c34c11cb2bd5f9fb1d29bf1b1f8f719560e5
bhupen <bhupensahoo200127@gmail.com>
2025-12-16 11:14:30 +0530

    supabase client setup

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,19 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
-
-import type { ClaimType } from "$lib/types";
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js'
 
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			claims: ClaimType
-		}
-		// interface PageData {}
+      supabase: SupabaseClient<any>
+      safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
+      session: Session | null
+      user: User | null
+    }
+    interface PageData {
+      session: Session | null
+    }
 		// interface PageState {}
 		// interface Platform {}
 	}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,52 @@
-import { redirect, type Handle } from "@sveltejs/kit";
-import "$lib/server/auth/client";
+import { createServerClient } from "@supabase/ssr";
+import { type Handle } from "@sveltejs/kit";
+
+import { SUPABASE_PUBLISHABLE_KEY } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import "$lib/server/auth/admin";
+import "$lib/server/auth/client";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  if (event.url.pathname === '/super-admin') {
-        throw redirect(307, '/super-admin/organizations');
+
+  event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+    cookies: {
+      getAll() {
+        return event.cookies.getAll()
+      },
+      setAll(cookiesToSet) {
+        /**
+         * Note: You have to add the `path` variable to the
+         * set and remove method due to sveltekit's cookie API
+         * requiring this to be set, setting the path to an empty string
+         * will replicate previous/standard behavior (https://kit.svelte.dev/docs/types#public-types-cookies)
+         */
+        cookiesToSet.forEach(({ name, value, options }) =>
+          event.cookies.set(name, value, { ...options, path: '/' })
+        )
+      },
     }
-    return resolve(event);
+  })
+
+   event.locals.safeGetSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession()
+    if (!session) {
+      return { session: null, user: null }
+    }
+    const {
+      data: { user },
+      error,
+    } = await event.locals.supabase.auth.getUser()
+    if (error) {
+      // JWT validation has failed
+      return { session: null, user: null }
+    }
+    return { session, user }
+  }
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range' || name === 'x-supabase-api-version'
+    },
+  })
 }
