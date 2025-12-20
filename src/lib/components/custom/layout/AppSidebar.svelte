98b2854c71bc8d8b8dc3b1e828db587d2004cf14
bhupen <bhupensahoo200127@gmail.com>
2025-12-20 10:57:00 +0530

    Add Sidebar fix and other routes

--- src/lib/components/custom/layout/AppSidebar.svelte
+++ src/lib/components/custom/layout/AppSidebar.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import { SIDEBAR_BY_ROLE } from '$lib/config/sidebar';
 	import { user } from '$lib/stores/user';
@@ -21,8 +20,8 @@
 		<Sidebar.MenuItem>
 			<Sidebar.MenuButton class="gap-x-4 h-10 px-4">
 				{#snippet child({ props })}
-					<a href="/" data-sveltekit-preload-data="hover" class="flex">
-						<ReceiptIcon />
+					<a href="/" {...props} data-sveltekit-preload-data="hover">
+						<ReceiptIcon class="size-4" />
 						<span class="font-semibold text-sm">Flex Bill</span>
 					</a>
 				{/snippet}
@@ -30,12 +29,13 @@
 		</Sidebar.MenuItem>
 	</Sidebar.Header>
 	<Sidebar.Content>
-		{#each items as group, index (group.title)}
-			<Sidebar.Group id={group.title}>
+		{#each items as group (group.title)}
+			<Sidebar.Group>
+				<Sidebar.GroupLabel>{group.title}</Sidebar.GroupLabel>
 				<Sidebar.GroupContent>
 					<Sidebar.Menu>
 						{#each group.items as item}
-							<Sidebar.MenuItem id={item.title}>
+							<Sidebar.MenuItem>
 								<Sidebar.MenuButton
 									tooltipContent={item.title}
 									isActive={isActiveRoute(item.url)}
@@ -59,16 +59,15 @@
 		<Sidebar.Menu>
 			<Sidebar.MenuItem>
 				<form action="/auth?/signout" method="post">
-					<Sidebar.MenuButton class="gap-x-4 h-10 px-4" tooltipContent="Logout">
-						{#snippet child(props)}
-							<Button
-								type="submit"
-								variant="secondary"
-								class="cursor-pointer w-full flex justify-start"
-							>
-								<LogOutIcon />
+					<Sidebar.MenuButton
+						class="gap-x-4 h-10 px-4 w-full justify-start"
+						tooltipContent="Logout"
+					>
+						{#snippet child({ props })}
+							<button {...props} type="submit">
+								<LogOutIcon class="size-4" />
 								<span>Logout</span>
-							</Button>
+							</button>
 						{/snippet}
 					</Sidebar.MenuButton>
 				</form>
---
+++ src/routes/org/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import AppSidebar from '$lib/components/custom/layout/AppSidebar.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+	let { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset class="bg-accent/70">
+		<main>
+			<Sidebar.Trigger />
+			{@render children?.()}
+		</main>
+	</Sidebar.Inset>
+</Sidebar.Provider>
